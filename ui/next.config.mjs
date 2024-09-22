d9bae5c02ce702be936886e8258c282c23223560
Akira0x01 <akira0x01@gmail.com>
2024-09-22 23:18:09 +0800

    modify ui settings

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,HEAD,PUT,PATCH,POST,DELETE" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ],
+        },
+      ];
+    },
+  };
 
 export default nextConfig;
