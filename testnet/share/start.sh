9506e5f02b6b54b8b1efa377f8396972d2310ac3
Akira0x01 <akira0x01@gmail.com>
2024-09-29 00:01:56 +0800

    update testnet

--- testnet/share/start.sh
+++ testnet/share/start.sh
@@ -24,14 +24,33 @@ TEMP_GENESIS=$CONF_DIR/tmp_genesis.json
 
 APP_CONFIG=$CONF_DIR/app.toml
 
-if [[ ! -d $DATA_DIR ]]; then
-    echo "Init $CHAIN with moniker=$MONIKER and chain-id=$CHAINID"
-    $CHAIND init "$MONIKER" --chain-id "$CHAINID" --home "$DATA_DIR"
-    cp $SHARE_DIR/genesis.json $GENESIS
-    cp $SHARE_DIR/client.toml $CONF_DIR
-    cp $SHARE_DIR/app.toml $CONF_DIR
-    cp $SHARE_DIR/config.toml $CONF_DIR
+if [ -f "$GENESIS" ]; then
+    echo "The file specified by GENESIS exists."
+    if [[ -z "${pruning}" ]]; then 
+        pruning="--pruning=nothing"
+    else
+        pruning=""
+        sed -i 's/pruning = "default"/pruning = "custom"/g' "$APP_CONFIG"
+        sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "5"/g' "$APP_CONFIG"
+        sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_CONFIG"
+    fi
+
+    echo "running $CHAIN with extra flags $EXTRA_FLAGS"
+    echo "starting $CHAIN node in background ..."
+    echo "./"$CHAIND" start "$pruning" --rpc.unsafe --keyring-backend test --home "$DATA_DIR" "$EXTRA_FLAGS" >"$DATA_DIR"/node.log"
+    $CHAIND start --rpc.unsafe \
+    --json-rpc.enable true --api.enable \
+    --keyring-backend test --home $DATA_DIR --chain-id $CHAINID $EXTRA_FLAGS \
+    --api.enabled-unsafe-cors
+    exit
 fi
+
+echo "Init $CHAIN with moniker=$MONIKER and chain-id=$CHAINID"
+$CHAIND init "$MONIKER" --chain-id "$CHAINID" --home "$DATA_DIR"
+cp $SHARE_DIR/genesis.json $GENESIS
+cp $SHARE_DIR/client.toml $CONF_DIR
+cp $SHARE_DIR/app.toml $CONF_DIR
+cp $SHARE_DIR/config.toml $CONF_DIR
 
 sed -i 's/prometheus = false/prometheus = true/g' $CONFIG
 sed -i 's/enable-indexer = false/enable-indexer = true/g' $APP_CONFIG
